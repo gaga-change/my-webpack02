03975d805e25e05b36217e59223a9b8feacde042
严俊东 <1172482914@qq.com>
2017-07-18 23:13:16 +0800

    添加dev-server 和 热加载

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 module.exports = {
@@ -9,7 +10,13 @@ module.exports = {
     filename: "[name].js",
     path: path.resolve(__dirname, './dist')
   },
+  devServer: {
+	  hot: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(), // 热加载
     new CleanWebpackPlugin(['dist']), // 打包前清空文件
     new HtmlWebpackPlugin({
       filename: 'index.html',
