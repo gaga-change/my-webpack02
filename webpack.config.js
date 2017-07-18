017d34e6b525f0672693738a3b94169ea3f1ed86
严俊东 <1172482914@qq.com>
2017-07-18 22:56:46 +0800

    添加html模板

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 module.exports = {
 	entry: {
 		app: './src/index.js'
@@ -7,5 +8,12 @@ module.exports = {
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, './dist')
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']), // 打包前清空文件
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './index.html'
+    })
+  ]
 }
